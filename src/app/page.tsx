67636cbce564a420108600ebb79dd4986a78e92b
Nweke Chidi <nwekechidigodwin460@gmail.com>
2024-06-14 15:40:22 +0100

    fix
    Signed-off-by: Nweke Chidi <nwekechidigodwin460@gmail.com>

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,5 +1,16 @@
-import Image from "next/image";
+import React from "react";
+import { Document, Page } from "react-pdf";
+import Image from "next/image"; // Optional if you still want to use Image
 
 export default function Home() {
-  return <section>Here</section>;
+  return (
+    <section className="w-full h-screen">
+    <iframe
+      src="/doc/document.pdf"
+      width="100%"
+      height="100%" // Using viewport height for full vertical space
+      frameBorder="0" // Remove border (corrected spelling)
+    ></iframe>
+  </section>
+  );
 }
