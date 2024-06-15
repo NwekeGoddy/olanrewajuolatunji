eeefd8d38fcbc466adcb10ba9b67b8ed3f633d71
Nweke Chidi <nwekechidigodwin460@gmail.com>
2024-06-15 20:23:07 +0100

    "Updated header component, removed document.pdf file, changed download link and icon, and modified iframe src in page component."
    Signed-off-by: Nweke Chidi <nwekechidigodwin460@gmail.com>

--- src/app/component/Header/Header.tsx
+++ src/app/component/Header/Header.tsx
@@ -5,47 +5,47 @@ import Link from "next/link";
 
 const Header = () => {
   return (
-    // <header className=" sticky z-20 top-0 h-[85.7px] md:h-[133px] py-7 md:py-12 bg-white backdrop-blur-3xl bg-opacity-100">
-    //   <div className="w-11/12  max-w-screen-xl  mx-auto bg-white ">
-    //     <nav className="flex items-center justify-between" aria-label="Global">
-    //       <div className="flex ">
-    //         <div className="-m-1.5 p-1.5">
-    //           <Link href="/">
-    //             <Image
-    //               className="w-[80px] md:w-[100px] h-auto"
-    //               src="/logo.jpg"
-    //               alt="Logo"
-    //               width={100}
-    //               height={37}
-    //               priority
-    //             />
-    //           </Link>
-    //         </div>
-    //       </div>
+    <header className=" sticky z-20 top-0 py-4 md:py-5 bg-white backdrop-blur-3xl bg-opacity-100">
+      <div className="w-11/12  max-w-screen-xl  mx-auto bg-white ">
+        <nav className="flex items-center justify-between" aria-label="Global">
+          <div className="flex ">
+            <div className="-m-1.5 p-1.5">
+              <Link href="/">
+                <Image
+                  className="w-[40px] md:w-[60px] h-auto"
+                  src="/logo.jpg"
+                  alt="Logo"
+                  width={70}
+                  height={37}
+                  priority
+                />
+              </Link>
+            </div>
+          </div>
 
-    //       <div className="flex items-center">
-    //         <div className="flex lg:justify-end gap-1 md:gap-4 mr-10 md:mr-0">
-    //           <Link href="/doc/document.pdf" passHref>
-    //             <div className="border border-[#D7DBF9] bg-[#F6F7FE] hover:bg-[#F6F7FE]/90 flex flex-row gap-2 py-3 px-5 rounded-lg cursor-pointer">
-    //               <p className="font-semibold text-sm text-hiven-blue">
-    //                 Download
-    //               </p>
-    //               <Image
-    //                 src="../icons/download-blue.svg"
-    //                 alt="Download Icon Button"
-    //                 width={16}
-    //                 height={16}
-    //                 className="w-[16px] h-[16px]"
-    //               />
-    //             </div>{" "}
-    //           </Link>
-    //         </div>
-    //       </div>
-    //     </nav>
-    //   </div>
-    // </header>
-    <header>
-        
+          <div className="flex items-center">
+            <div className="flex lg:justify-end gap-1 md:gap-4 ">
+              <Link
+                href="https://drive.google.com/uc?export=download&id=1Vdbg2Kayyw9bFuH1DXFeEyN7zWyemitO"
+                passHref
+              >
+                <div className="border border-[#957628]/50 bg-[#957628] hover:bg-[#957628]/90 flex flex-row gap-2 py-2 md:py-3 px-3 md:px-5 rounded-lg cursor-pointer">
+                  <p className="font-semibold text-xs md:text-sm text-white">
+                    Download
+                  </p>
+                  <Image
+                    src="../icons/download-blue.svg"
+                    alt="Download Icon Button"
+                    width={16}
+                    height={16}
+                    className="w-[16px] h-[16px]"
+                  />
+                </div>{" "}
+              </Link>
+            </div>
+          </div>
+        </nav>
+      </div>
     </header>
   );
 };
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,16 +1,21 @@
 import React from "react";
-import { Document, Page } from "react-pdf";
 import Image from "next/image"; // Optional if you still want to use Image
 
 export default function Home() {
   return (
-    <section className="w-full h-screen">
-    <iframe
-      src="https://drive.google.com/drive/folders/1FkLBV463-NJD5vonGBhjaB9DBHo2qXjC"
-      width="100%"
-      height="100%" // Using viewport height for full vertical space
-      frameBorder="0" // Remove border (corrected spelling)
-    ></iframe>
-  </section>
+    <section className="relative w-full h-screen">
+      <iframe
+        src="https://drive.google.com/file/d/1Vdbg2Kayyw9bFuH1DXFeEyN7zWyemitO/preview"
+        width="100%"
+        height="100%"
+        frameBorder="0"
+      ></iframe>
+      <a
+        href="https://drive.google.com/uc?export=download&id=1Vdbg2Kayyw9bFuH1DXFeEyN7zWyemitO"
+        className="absolute bottom-4 right-4 bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded"
+      >
+        Download PDF
+      </a>
+    </section>
   );
 }
